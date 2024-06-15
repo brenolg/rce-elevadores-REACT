dcef89ab5ee36a8913b48b82fcfb60cd470cd01a
Breno.garrido <breno.garrido@base2.com.br>
2024-06-15 14:18:31 -0300

    deploy fix

--- src/pages/Home/Differential/Differential.jsx
+++ src/pages/Home/Differential/Differential.jsx
@@ -9,7 +9,7 @@ export default function Differential() {
       <div className="differential-cards-container">
         <div className="differential-card">
           <h3 className="differential-card-title">Acompanhamento do Engenheiro</h3>
-          <p className="differential-card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium magni fuga, repudiandae ab eos dicta ad at, molestias culpa voluptatibus quo aliquid perferendis debitis recusandae optio tenetur ducimus? Beatae, laboriosam.</p>
+          <p className="differential-card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium magni fuga, repudiandae ab eos dicta ad at, molestias culpa voluptatibus quo aliquid perferendis debitis recusandae optio tenetur ducimus? Beatae, laboriosam. xx</p>
         </div>
         <div className="differential-card">
           <h3 className="differential-card-title">Alta qualidade</h3>
